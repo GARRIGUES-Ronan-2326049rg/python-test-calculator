a5f3884dabc1469876d4462198a94c16d3364482
Ronan garrigues <ronan.garrigues@etu.univ-amu.fr>
2025-12-17 11:46:22 +0100

    fix

--- src/calculator.py
+++ src/calculator.py
@@ -11,5 +11,5 @@ def divide(a,b):
 	checkInputs(a,b)
 	return a/b
 def checkInputs(a,b):
-	if not instance(a,(int,float)) or not instance(b,(int,float)):
+	if not isinstance(a,(int,float)) or not isinstance(b,(int,float)):
 		raise TypeError("Inputs must be either int or float!")
